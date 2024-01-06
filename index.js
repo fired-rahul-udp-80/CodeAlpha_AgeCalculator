c7c4250c2edba2ffd43ef775e3a8c026c4531a37
fired-rahul-udp-80 <152783083+fired-rahul-udp-80@users.noreply.github.com>
2024-01-07 01:11:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+function ageCalculator(){
+    let today = new Date();
+    let inputDate = new Date(document.getElementById('date-input').value);
+    let birthMonth, birthDate,birthYear;
+
+    let birthDetails ={
+        date: inputDate.getDate(),
+        month : inputDate.getMonth(),
+        year : inputDate.getFullYear()
+    }
+
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth();
+    let currentDate = today.getDate();
+
+    leapCheaker(currentYear);
+
+    if(birthDetails.year > currentYear || 
+        (birthDetails.month > currentMonth &&
+        birthDetails.year == currentYear) ||
+        (birthDetails.date > currentDate &&
+            birthDetails.month == currentMonth &&
+            birthDetails.year == currentYear)
+        
+        )
+        {
+            alert('Not Born Yet');
+            return;
+        }
+
+        birthYear = currentYear - birthDetails.year;
+        if(currentMonth >= birthDetails.month){
+            birthMonth = currentMonth - birthDetails.month;
+        }
+        else{
+            birthYear--;
+            birthMonth = 12 + currentMonth - birthDetails.month;
+        }
+
+        if( currentDate >= birthDetails.date){
+            birthDate = currentDate - birthDetails.date;
+        }
+        else{
+            birthMonth--;
+            let days = months[currentMonth - 2];
+            birthDate = days + currentDate - birthDetails.date;
+            if(birthMonth < 0){
+                birthMonth = 11;
+                birthYear--;
+            }
+        }
+
+        document.getElementById('d-box').innerHTML = birthDate;
+        document.getElementById('m-box').innerHTML = birthMonth;
+        document.getElementById('y-box').innerHTML = birthYear;
+        console.log(birthYear,birthMonth,birthDate);
+
+}
+
+function leapCheaker(){
+    let year;
+    if(year % 4 == 0 || year % 100 ==0 && year % 400 == 0){
+        months[1] = 29;
+    }
+    else{
+        months[1]= 28;
+    }
+}
